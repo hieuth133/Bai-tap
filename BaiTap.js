39498ff4adda2635480d9c986e3cdc12f000a2e2
HieuTrinh <hieudaic@gmail.com>
2020-05-16 19:49:26 +0700

    second change

--- BaiTap.js
+++ BaiTap.js
@@ -1,9 +1,1 @@
-// ## **Thực hành**
-
-// 1. Hãy thử thay đổi 2 giá trị số `5` và `10` thành các số khác và chạy lại chương trình.
-// 2. Khai báo biến có tên `c` với giá trị là tổng của `a` và `b`
-// 3. Hiển thị giá trị của `c`
-
-var a = 8;
-var b = 11;
-console.log(a + b);
+// 
